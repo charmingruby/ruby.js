11846899f1cf13b4dffd8b5abbedcd7851a0c1aa
charmingruby <mrrubycanal3105@gmail.com>
2022-06-05 15:14:43 -0300

    initial loopings

---
+++ lvl-1/loopings/exercise/for-dom/assets/js/main.js
@@ -0,0 +1,12 @@
+const elements = [
+    {tag: 'p', text: 'Sentence 1'},
+    {tag: 'div', text: 'Sentence 2'},
+    {tag: 'footer', text: 'Sentence 3'},
+    {tag: 'section', text: 'Sentence 4'}
+];
+
+const section = document.querySelector('.container');
+
+for(let c = 0; c < elements.length ;c++) {
+
+}
---
+++ lvl-1/loopings/for-classic/main.js
@@ -0,0 +1,5 @@
+list = ['First', 'Second', 'Third'];
+
+for(i = 0; i < list.length ;i++){
+    console.log(`Array index: ${i} = ${list[i]}`)
+}
